891fc5c4427607401c477ce5ed750d10abe106ea
Mary Strodl <ipadlover8322@gmail.com>
2023-08-28 21:13:38 -0400

    CSHUserSig: need mediawiki

--- CSHUserSig.php
+++ CSHUserSig.php
@@ -4,8 +4,10 @@
 
 // PHP 7 Compatability ~ <3 mbillow 03/14/2017
 
+// MediaWiki 1.36 compat ~ <3 mstrodl 2023-01-25
+
 global $wgHooks, $wgOut;
-$wgHooks['ParserBeforeStrip'][]      = 'parseUserSignatures';
+$wgHooks['ParserBeforeInternalParse'][]      = 'parseUserSignatures';
 $wgExtensionCredits['parserhook'][] = array(
 	'version'     => '0.0.1',
 	'name'        => 'CSH Usersig',
@@ -16,7 +18,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'descriptionmsg' => 'cshuserlink-desc',
 );
 
-function parseUserSignatures($parser, $text) {
+function parseUserSignatures( &$parser, &$text, &$strip_state ) {
 	while (preg_match('/\^\^\^(.+?)\^\^\^/', $text, $matches)) {
 		$user = getUserInfoFromText($matches[1]);
 		if($user != 0){
@@ -36,22 +38,23 @@ function parseUserSignatures($parser, $text) {
 	return true;
 }
 
-function getUserInfoFromText($name) {
+function getLdapClient() {
 	if(!isset($GLOBALS['ldap_ds'])){
-		$GLOBALS['ldap_ds'] = ldap_connect('ipa10-nrh.csh.rit.edu');
-		ldap_bind($GLOBALS['ldap_ds'],'cn=wiki,ou=Apps,dc=csh,dc=rit,dc=edu','');
+		$GLOBALS['ldap_ds'] = ldap_connect('ldaps://ipa11-nrh.csh.rit.edu');
+    ldap_bind($GLOBALS['ldap_ds'],'krbprincipalname=wiki/yasuko.csh.rit.edu@CSH.RIT.EDU,cn=services,cn=accounts,dc=csh,dc=rit,dc=edu',$GLOBALS['csh_wiki_ldap_password']);
 	}
-	$sr = ldap_list($GLOBALS['ldap_ds'],'ou=Users,dc=csh,dc=rit,dc=edu',"(displayName=*$name*)",array('uid','sn','givenname','cn'));
+}
+
+function getUserInfoFromText($name) {
+	getLdapClient();
+	$sr = ldap_list($GLOBALS['ldap_ds'],'cn=users,cn=accounts,dc=csh,dc=rit,dc=edu',"(displayName=*$name*)",array('uid','cn'));
 	$results = ldap_get_entries($GLOBALS['ldap_ds'],$sr);
 	if(!isset($results[0])) return 0;
 	return array('uid'=>$results[0]['uid'][0],'name'=>$results[0]['cn'][0]);
 }
 function getUserInfoFromUID($name) {
-	if(!isset($GLOBALS['ldap_ds'])){
-		$GLOBALS['ldap_ds'] = ldap_connect('ipa10-nrh.csh.rit.edu');
-		ldap_bind($GLOBALS['ldap_ds'],'cn=wiki,ou=Apps,dc=csh,dc=rit,dc=edu','');
-	}
-	$sr = ldap_list($GLOBALS['ldap_ds'],'ou=Users,dc=csh,dc=rit,dc=edu',"(uid=$name)",array('uid','sn','givenname','cn'));
+	getLdapClient();
+	$sr = ldap_list($GLOBALS['ldap_ds'],'cn=users,cn=accounts,dc=csh,dc=rit,dc=edu',"(uid=$name)",array('uid','cn'));
 	$results = ldap_get_entries($GLOBALS['ldap_ds'],$sr);
 	if(!isset($results[0])) return 0;
 	return array('uid'=>$results[0]['uid'][0],'name'=>$results[0]['cn'][0]);
